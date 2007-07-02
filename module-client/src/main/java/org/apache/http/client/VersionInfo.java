e31743c34edd8f8f58472b8c44d09e7c41ed9daf
Oleg Kalnichevski <olegk@apache.org>
2007-07-02 16:12:43 +0000

    Removed flawed revision attribute
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/httpclient/trunk@552527 13f79535-47bb-0310-9956-ffa450edef68

--- module-client/src/main/java/org/apache/http/client/VersionInfo.java
+++ module-client/src/main/java/org/apache/http/client/VersionInfo.java
@@ -3,17 +3,13 @@ package org.apache.http.client;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class VersionInfo {
 
     private static final String RESOURCE = "org/apache/http/client/version.properties";
-    private static final String REV = "$Revision$";
 
     private static Properties RELEASE_PROPERTIES;
     private static String RELEASE_VERSION;
-    private static Long REV_VERSION;
 
     private static Properties getReleaseProperties() {
         if (RELEASE_PROPERTIES == null) {
@@ -50,25 +46,6 @@ public class VersionInfo {
             }
         }
         return RELEASE_VERSION;
-    }
-    
-    public static long getRevision() {
-        if (REV_VERSION == null) {
-            Pattern p = Pattern.compile("\\$Revision: (\\d*) \\$");
-            Matcher m = p.matcher(REV);
-            if (m.matches()) {
-                String s = m.group(1);
-                try {
-                    REV_VERSION = new Long(Long.parseLong(s));
-                } catch (NumberFormatException ex) {
-                    // shamelessly munch this exception
-                }
-            }
-            if (REV_VERSION == null) {
-                REV_VERSION = new Long(-1);
-            }
-        }
-        return REV_VERSION.intValue();
     }
     
 }
