0c97d4aa7014786ae03f4eed267ebc1e0ad466c2
Oleg Kalnichevski <olegk@apache.org>
2007-07-07 10:33:42 +0000

    Added an example that uses HttpClient to execute an HTTP request over a secure connection tunneled through an authenticating proxy
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/httpclient/trunk@554174 13f79535-47bb-0310-9956-ffa450edef68

---
+++ module-client/src/examples/org/apache/http/examples/client/ClientProxyAuthentication.java
@@ -0,0 +1,82 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ * ====================================================================
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * [Additional notices, if required by prior licensing conditions]
+ *
+ */
+
+package org.apache.http.examples.client;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.RoutedRequest;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.HttpRoute;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * A simple example that uses HttpClient to execute an HTTP request 
+ * over a secure connection tunneled through an authenticating proxy. 
+ */
+public class ClientProxyAuthentication {
+
+    public static void main(String[] args) throws Exception {
+        
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+
+        httpclient.getState().setCredentials(
+                new AuthScope("localhost", 8080), 
+                new UsernamePasswordCredentials("username", "password"));
+
+        HttpHost targetHost = new HttpHost("www.verisign.com", 443, "https"); 
+        HttpHost proxy = new HttpHost("localhost", 8080); 
+        HttpRoute route = new HttpRoute(targetHost, null, proxy, true);
+
+        HttpGet httpget = new HttpGet("/");
+        
+        RoutedRequest routedReq = new RoutedRequest.Impl(httpget, route); 
+
+        System.out.println("executing request: " + httpget.getRequestLine());
+        System.out.println("using route: " + route);
+        
+        HttpResponse response = httpclient.execute(routedReq, null);
+        HttpEntity entity = response.getEntity();
+
+        System.out.println("----------------------------------------");
+        System.out.println(response.getStatusLine());
+        if (entity != null) {
+            System.out.println("Response content length: " + entity.getContentLength());
+            System.out.println("Chunked?: " + entity.isChunked());
+        }
+        if (entity != null) {
+            entity.consumeContent();
+        }
+    }
+}
