8ad4cebc82d51bb0428da5c5019c93ccea96f2fa
Oleg Kalnichevski <olegk@apache.org>
2007-10-07 14:37:22 +0000

    Added protocol test cases to TestAll test suite
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/httpclient/trunk@582637 13f79535-47bb-0310-9956-ffa450edef68

--- module-client/src/test/java/org/apache/http/client/protocol/TestAllProtocol.java
+++ module-client/src/test/java/org/apache/http/client/protocol/TestAllProtocol.java
@@ -28,40 +28,28 @@
  *
  */
 
-package org.apache.http.client;
+package org.apache.http.client.protocol;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.apache.http.client.protocol.TestAllProtocol;
-import org.apache.http.conn.TestAllConn;
-import org.apache.http.conn.ssl.TestAllSSL;
-import org.apache.http.cookie.TestAllCookie;
-import org.apache.http.impl.client.TestAllHttpClientImpl;
-import org.apache.http.impl.conn.TestAllConnImpl;
-import org.apache.http.impl.cookie.TestAllCookieImpl;
+public class TestAllProtocol extends TestCase {
 
-public class TestAll extends TestCase {
-
-    public TestAll(String testName) {
+    public TestAllProtocol(String testName) {
         super(testName);
     }
 
     public static Test suite() {
         TestSuite suite = new TestSuite();
-        suite.addTest(TestAllCookie.suite());
-        suite.addTest(TestAllCookieImpl.suite());
-        suite.addTest(TestAllHttpClientImpl.suite());
-        suite.addTest(TestAllConn.suite());
-        suite.addTest(TestAllConnImpl.suite());
-        suite.addTest(TestAllSSL.suite());        
-        suite.addTest(TestAllProtocol.suite());        
+
+        suite.addTest(TestCookie2Support.suite());
+
         return suite;
     }
 
     public static void main(String args[]) {
-        String[] testCaseName = { TestAll.class.getName() };
+        String[] testCaseName = { TestAllProtocol.class.getName() };
         junit.textui.TestRunner.main(testCaseName);
     }
 
