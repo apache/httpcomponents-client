08f2e11f1a0142cc1fe0e16d80ea8b18a3cf2cab
Oleg Kalnichevski <olegk@apache.org>
2008-02-01 18:20:39 +0000

    Multipart/form HTTP entity example
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@617589 13f79535-47bb-0310-9956-ffa450edef68

---
+++ module-httpmime/src/examples/org/apache/http/examples/client/mime/ClientMultipartFormPost.java
@@ -0,0 +1,82 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ * ====================================================================
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ * 
+ */
+
+package org.apache.http.examples.client.mime;
+
+import java.io.File;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.mime.MultipartFormHttpEntity;
+import org.apache.http.client.mime.content.FileBody;
+import org.apache.http.client.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * Example how to use unbuffered chunk-encoded POST request.
+ */
+public class ClientMultipartFormPost {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1)  {
+            System.out.println("File path not given");
+            System.exit(1);
+        }
+        HttpClient httpclient = new DefaultHttpClient();
+
+        HttpPost httppost = new HttpPost("http://localhost:8080" +
+                "/servlets-examples/servlet/RequestInfoExample");
+
+        FileBody bin = new FileBody(new File(args[0]));
+        StringBody comment = new StringBody("A binary file of some kind");
+
+        MultipartFormHttpEntity reqEntity = new MultipartFormHttpEntity();
+        reqEntity.addPart("bin", bin);
+        reqEntity.addPart("comment", comment);
+        
+        httppost.setEntity(reqEntity);
+        
+        System.out.println("executing request " + httppost.getRequestLine());
+        HttpResponse response = httpclient.execute(httppost);
+        HttpEntity resEntity = response.getEntity();
+
+        System.out.println("----------------------------------------");
+        System.out.println(response.getStatusLine());
+        if (resEntity != null) {
+            System.out.println("Response content length: " + resEntity.getContentLength());
+            System.out.println("Chunked?: " + resEntity.isChunked());
+        }
+        if (resEntity != null) {
+            resEntity.consumeContent();
+        }
+    }
+    
+}
