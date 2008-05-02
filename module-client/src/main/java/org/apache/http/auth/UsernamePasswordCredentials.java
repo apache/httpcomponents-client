2b96bbd67851169687d0d3ac39186de57434ccc6
Sebastian Bazley <sebb@apache.org>
2008-05-02 01:28:39 +0000

    Make field final so it is guaranteed to be published
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@652728 13f79535-47bb-0310-9956-ffa450edef68

--- module-client/src/main/java/org/apache/http/auth/UsernamePasswordCredentials.java
+++ module-client/src/main/java/org/apache/http/auth/UsernamePasswordCredentials.java
@@ -64,6 +64,7 @@ public class UsernamePasswordCredentials implements Credentials {
             this.password = usernamePassword.substring(atColon + 1);
         } else {
             this.userName = usernamePassword;
+            this.password = null;
         }
     }
 
@@ -88,13 +89,13 @@ public class UsernamePasswordCredentials implements Credentials {
     /**
      * User name.
      */
-    private String userName;
+    private final String userName;
 
 
     /**
      * Password.
      */
-    private String password;
+    private final String password;
 
 
     // ------------------------------------------------------------- Properties
