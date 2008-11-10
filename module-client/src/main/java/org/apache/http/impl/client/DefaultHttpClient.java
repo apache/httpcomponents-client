b891c25b6f659dfc82323b147fdc3148cb207675
Oleg Kalnichevski <olegk@apache.org>
2008-11-10 19:38:43 +0000

    Removed IgnoreConnectMethod private class, which is no longer needed; updated User-Agent string
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@712727 13f79535-47bb-0310-9956-ffa450edef68

--- module-client/src/main/java/org/apache/http/impl/client/DefaultHttpClient.java
+++ module-client/src/main/java/org/apache/http/impl/client/DefaultHttpClient.java
@@ -31,12 +31,7 @@
 
 package org.apache.http.impl.client;
 
-import java.io.IOException;
-
 import org.apache.http.ConnectionReuseStrategy;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpVersion;
 import org.apache.http.auth.AuthSchemeRegistry;
 import org.apache.http.client.AuthenticationHandler;
@@ -145,7 +140,7 @@ public class DefaultHttpClient extends AbstractHttpClient {
         final String release = (vi != null) ?
             vi.getRelease() : VersionInfo.UNAVAILABLE;
         HttpProtocolParams.setUserAgent(params, 
-                "Apache-HttpClient/" + release + " (java 1.4)");
+                "Apache-HttpClient/" + release + " (java 1.5)");
 
         return params;
     }
@@ -271,9 +266,9 @@ public class DefaultHttpClient extends AbstractHttpClient {
         httpproc.addInterceptor(new RequestDefaultHeaders());
         // Required protocol interceptors
         httpproc.addInterceptor(new RequestContent());
-        httpproc.addInterceptor(new IgnoreConnectMethod(new RequestTargetHost()));
+        httpproc.addInterceptor(new RequestTargetHost());
         // Recommended protocol interceptors
-        httpproc.addInterceptor(new IgnoreConnectMethod(new RequestConnControl()));
+        httpproc.addInterceptor(new RequestConnControl());
         httpproc.addInterceptor(new RequestUserAgent());
         httpproc.addInterceptor(new RequestExpectContinue());
         // HTTP state management interceptors
@@ -332,28 +327,6 @@ public class DefaultHttpClient extends AbstractHttpClient {
     @Override
     protected UserTokenHandler createUserTokenHandler() {
         return new DefaultUserTokenHandler();
-    }
-    
-    // FIXME: remove this class when protocol interceptors in HttpCore
-    // are updated to ignore CONNECT methods
-    static class IgnoreConnectMethod implements HttpRequestInterceptor {
-
-        private final HttpRequestInterceptor interceptor;
-        
-        public IgnoreConnectMethod(final HttpRequestInterceptor interceptor) {
-            super();
-            this.interceptor = interceptor;
-        }
-        
-        public void process(
-                final HttpRequest request, 
-                final HttpContext context) throws HttpException, IOException {
-            String method = request.getRequestLine().getMethod();
-            if (!method.equalsIgnoreCase("CONNECT")) {
-                this.interceptor.process(request, context);
-            }
-        }
-        
     }
     
 } // class DefaultHttpClient
