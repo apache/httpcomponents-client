65b8b329eccf731a972d5d82f41ee08755228768
Oleg Kalnichevski <olegk@apache.org>
2009-02-28 12:05:44 +0000

    Cleaned up execute direct and execute via proxy examples
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@748828 13f79535-47bb-0310-9956-ffa450edef68

--- module-client/src/examples/org/apache/http/examples/client/ClientExecuteDirect.java
+++ module-client/src/examples/org/apache/http/examples/client/ClientExecuteDirect.java
@@ -31,7 +31,6 @@
 
 package org.apache.http.examples.client;
 
-
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -43,7 +42,6 @@ import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.scheme.SocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicHttpRequest;
@@ -52,134 +50,53 @@ import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.util.EntityUtils;
 
-
-
 /**
- * How to send a request directly using {@link HttpClient HttpClient}.
- *
- *
- *
- * <!-- empty lines above to avoid 'svn diff' context problems -->
- * @version $Revision$
- *
+ * How to send a request directly using {@link HttpClient}.
+ * 
  * @since 4.0
  */
 public class ClientExecuteDirect {
 
-    /**
-     * The default parameters.
-     * Instantiated in {@link #setup setup}.
-     */
-    private static HttpParams defaultParameters = null;
+    public static void main(String[] args) throws Exception {
 
-    /**
-     * The scheme registry.
-     * Instantiated in {@link #setup setup}.
-     */
-    private static SchemeRegistry supportedSchemes;
+        HttpHost target = new HttpHost("www.apache.org", 80, "http");
 
-
-    /**
-     * Main entry point to this example.
-     *
-     * @param args      ignored
-     */
-    public final static void main(String[] args)
-        throws Exception {
-
-        final HttpHost target = new HttpHost("issues.apache.org", 80, "http");
-
-        setup(); // some general setup
-
-        HttpClient client = createHttpClient();
-
-        HttpRequest req = createRequest();
-
-        System.out.println("executing request to " + target);
-        HttpEntity entity = null;
-        try {
-            HttpResponse rsp = client.execute(target, req);
-            entity = rsp.getEntity();
-
-            System.out.println("----------------------------------------");
-            System.out.println(rsp.getStatusLine());
-            Header[] headers = rsp.getAllHeaders();
-            for (int i=0; i<headers.length; i++) {
-                System.out.println(headers[i]);
-            }
-            System.out.println("----------------------------------------");
-
-            if (entity != null) {
-                System.out.println(EntityUtils.toString(rsp.getEntity()));
-            }
-
-        } finally {
-            // If we could be sure that the stream of the entity has been
-            // closed, we wouldn't need this code to release the connection.
-            // However, EntityUtils.toString(...) can throw an exception.
-
-            // if there is no entity, the connection is already released
-            if (entity != null)
-                entity.consumeContent(); // release connection gracefully
-        }
-    } // main
-
-
-    private final static HttpClient createHttpClient() {
-
-        ClientConnectionManager ccm =
-            new ThreadSafeClientConnManager(getParams(), supportedSchemes);
-        //  new SingleClientConnManager(getParams(), supportedSchemes);
-
-        DefaultHttpClient dhc =
-            new DefaultHttpClient(ccm, getParams());
-
-        return dhc;
-    }
-
-
-    /**
-     * Performs general setup.
-     * This should be called only once.
-     */
-    private final static void setup() {
-
-        supportedSchemes = new SchemeRegistry();
+        // general setup
+        SchemeRegistry supportedSchemes = new SchemeRegistry();
 
         // Register the "http" protocol scheme, it is required
         // by the default operator to look up socket factories.
-        SocketFactory sf = PlainSocketFactory.getSocketFactory();
-        supportedSchemes.register(new Scheme("http", sf, 80));
+        supportedSchemes.register(new Scheme("http", 
+                PlainSocketFactory.getSocketFactory(), 80));
 
         // prepare parameters
         HttpParams params = new BasicHttpParams();
         HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
         HttpProtocolParams.setContentCharset(params, "UTF-8");
         HttpProtocolParams.setUseExpectContinue(params, true);
-        defaultParameters = params;
 
-    } // setup
+        ClientConnectionManager connMgr = new ThreadSafeClientConnManager(params, 
+                supportedSchemes);
+        DefaultHttpClient httpclient = new DefaultHttpClient(connMgr, params);
 
+        HttpRequest req = new BasicHttpRequest("GET", "/", HttpVersion.HTTP_1_1);
 
-    private final static HttpParams getParams() {
-        return defaultParameters;
+        System.out.println("executing request to " + target);
+
+        HttpResponse rsp = httpclient.execute(target, req);
+        HttpEntity entity = rsp.getEntity();
+
+        System.out.println("----------------------------------------");
+        System.out.println(rsp.getStatusLine());
+        Header[] headers = rsp.getAllHeaders();
+        for (int i = 0; i < headers.length; i++) {
+            System.out.println(headers[i]);
+        }
+        System.out.println("----------------------------------------");
+
+        if (entity != null) {
+            System.out.println(EntityUtils.toString(entity));
+        }
     }
 
-
-    /**
-     * Creates a request to execute in this example.
-     *
-     * @return  a request without an entity
-     */
-    private final static HttpRequest createRequest() {
-
-        HttpRequest req = new BasicHttpRequest
-            ("GET", "/", HttpVersion.HTTP_1_1);
-          //("OPTIONS", "*", HttpVersion.HTTP_1_1);
-
-        return req;
-    }
-
-
-} // class ClientExecuteDirect
-
+}
--- module-client/src/examples/org/apache/http/examples/client/ClientExecuteProxy.java
+++ module-client/src/examples/org/apache/http/examples/client/ClientExecuteProxy.java
@@ -31,7 +31,6 @@
 
 package org.apache.http.examples.client;
 
-
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -44,7 +43,6 @@ import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.scheme.SocketFactory;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
@@ -54,142 +52,59 @@ import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.util.EntityUtils;
 
-
-
 /**
- * How to send a request via proxy using {@link HttpClient HttpClient}.
- *
- *
- *
- * <!-- empty lines above to avoid 'svn diff' context problems -->
- * @version $Revision$
+ * How to send a request via proxy using {@link HttpClient}.
  *
  * @since 4.0
  */
 public class ClientExecuteProxy {
 
-    /**
-     * The default parameters.
-     * Instantiated in {@link #setup setup}.
-     */
-    private static HttpParams defaultParameters = null;
-
-    /**
-     * The scheme registry.
-     * Instantiated in {@link #setup setup}.
-     */
-    private static SchemeRegistry supportedSchemes;
-
-
-    /**
-     * Main entry point to this example.
-     *
-     * @param args      ignored
-     */
-    public final static void main(String[] args)
-        throws Exception {
+    public static void main(String[] args)throws Exception {
 
         // make sure to use a proxy that supports CONNECT
-        final HttpHost target =
-            new HttpHost("issues.apache.org", 443, "https");
-        final HttpHost proxy =
-            new HttpHost("127.0.0.1", 8666, "http");
+        HttpHost target = new HttpHost("issues.apache.org", 443, "https");
+        HttpHost proxy = new HttpHost("127.0.0.1", 8080, "http");
 
-        setup(); // some general setup
-
-        HttpClient client = createHttpClient();
-
-        client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
-
-        HttpRequest req = createRequest();
-
-        System.out.println("executing request to " + target + " via " + proxy);
-        HttpEntity entity = null;
-        try {
-            HttpResponse rsp = client.execute(target, req);
-            entity = rsp.getEntity();
-
-            System.out.println("----------------------------------------");
-            System.out.println(rsp.getStatusLine());
-            Header[] headers = rsp.getAllHeaders();
-            for (int i=0; i<headers.length; i++) {
-                System.out.println(headers[i]);
-            }
-            System.out.println("----------------------------------------");
-
-            if (rsp.getEntity() != null) {
-                System.out.println(EntityUtils.toString(rsp.getEntity()));
-            }
-
-        } finally {
-            // If we could be sure that the stream of the entity has been
-            // closed, we wouldn't need this code to release the connection.
-            // However, EntityUtils.toString(...) can throw an exception.
-
-            // if there is no entity, the connection is already released
-            if (entity != null)
-                entity.consumeContent(); // release connection gracefully
-        }
-    } // main
-
-
-    private final static HttpClient createHttpClient() {
-
-        ClientConnectionManager ccm =
-            new ThreadSafeClientConnManager(getParams(), supportedSchemes);
-        //  new SingleClientConnManager(getParams(), supportedSchemes);
-
-        DefaultHttpClient dhc =
-            new DefaultHttpClient(ccm, getParams());
-
-        return dhc;
-    }
-
-
-    /**
-     * Performs general setup.
-     * This should be called only once.
-     */
-    private final static void setup() {
-
-        supportedSchemes = new SchemeRegistry();
+        // general setup
+        SchemeRegistry supportedSchemes = new SchemeRegistry();
 
         // Register the "http" and "https" protocol schemes, they are
         // required by the default operator to look up socket factories.
-        SocketFactory sf = PlainSocketFactory.getSocketFactory();
-        supportedSchemes.register(new Scheme("http", sf, 80));
-        sf = SSLSocketFactory.getSocketFactory();
-        supportedSchemes.register(new Scheme("https", sf, 80));
+        supportedSchemes.register(new Scheme("http", 
+                PlainSocketFactory.getSocketFactory(), 80));
+        supportedSchemes.register(new Scheme("https", 
+                SSLSocketFactory.getSocketFactory(), 443));
 
         // prepare parameters
         HttpParams params = new BasicHttpParams();
         HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
         HttpProtocolParams.setContentCharset(params, "UTF-8");
         HttpProtocolParams.setUseExpectContinue(params, true);
-        defaultParameters = params;
 
-    } // setup
+        ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, 
+                supportedSchemes);
 
+        DefaultHttpClient httpclient = new DefaultHttpClient(ccm, params);
 
-    private final static HttpParams getParams() {
-        return defaultParameters;
+        httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+
+        HttpRequest req = new BasicHttpRequest("GET", "/", HttpVersion.HTTP_1_1);
+
+        System.out.println("executing request to " + target + " via " + proxy);
+        HttpResponse rsp = httpclient.execute(target, req);
+        HttpEntity entity = rsp.getEntity();
+
+        System.out.println("----------------------------------------");
+        System.out.println(rsp.getStatusLine());
+        Header[] headers = rsp.getAllHeaders();
+        for (int i = 0; i<headers.length; i++) {
+            System.out.println(headers[i]);
+        }
+        System.out.println("----------------------------------------");
+
+        if (entity != null) {
+            System.out.println(EntityUtils.toString(entity));
+        }
     }
 
-
-    /**
-     * Creates a request to execute in this example.
-     *
-     * @return  a request without an entity
-     */
-    private final static HttpRequest createRequest() {
-
-        HttpRequest req = new BasicHttpRequest
-            ("GET", "/", HttpVersion.HTTP_1_1);
-          //("OPTIONS", "*", HttpVersion.HTTP_1_1);
-
-        return req;
-    }
-
-
-} // class ClientExecuteProxy
-
+}
