087d6af97dab2ee19fc01181c4f15fb85e9a3199
Sebastian Bazley <sebb@apache.org>
2009-03-18 01:32:47 +0000

    Fix alignment
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@755453 13f79535-47bb-0310-9956-ffa450edef68

--- httpclient/src/main/java/org/apache/http/cookie/SM.java
+++ httpclient/src/main/java/org/apache/http/cookie/SM.java
@@ -40,7 +40,7 @@ package org.apache.http.cookie;
 public interface SM {
 
     public static final String COOKIE            = "Cookie";
-    public static final String COOKIE2          = "Cookie2";
+    public static final String COOKIE2           = "Cookie2";
     public static final String SET_COOKIE        = "Set-Cookie";
     public static final String SET_COOKIE2       = "Set-Cookie2";
        
