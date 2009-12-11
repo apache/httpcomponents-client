4096a39651fbff276bce539fa47a4769db03f87d
Oleg Kalnichevski <olegk@apache.org>
2009-12-11 23:34:23 +0000

    HTTPCLIENT-901: support for execution context aware auth schemes
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@889860 13f79535-47bb-0310-9956-ffa450edef68

--- httpclient/src/main/java/org/apache/http/client/protocol/RequestProxyAuthentication.java
+++ httpclient/src/main/java/org/apache/http/client/protocol/RequestProxyAuthentication.java
@@ -33,6 +33,7 @@ import org.apache.http.annotation.Immutable;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.Header;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
@@ -40,6 +41,7 @@ import org.apache.http.auth.AUTH;
 import org.apache.http.auth.AuthScheme;
 import org.apache.http.auth.AuthState;
 import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.ContextAwareAuthScheme;
 import org.apache.http.auth.Credentials;
 import org.apache.http.protocol.HttpContext;
 
@@ -90,7 +92,14 @@ public class RequestProxyAuthentication implements HttpRequestInterceptor {
         }
         if (authState.getAuthScope() != null || !authScheme.isConnectionBased()) {
             try {
-                request.addHeader(authScheme.authenticate(creds, request));
+                Header header;
+                if (authScheme instanceof ContextAwareAuthScheme) {
+                    header = ((ContextAwareAuthScheme) authScheme).authenticate(
+                            creds, request, context);
+                } else {
+                    header = authScheme.authenticate(creds, request);
+                }
+                request.addHeader(header);
             } catch (AuthenticationException ex) {
                 if (this.log.isErrorEnabled()) {
                     this.log.error("Proxy authentication error: " + ex.getMessage());
