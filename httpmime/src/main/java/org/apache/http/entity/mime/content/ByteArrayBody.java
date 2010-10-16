354171b34447e7700acb2d93173f69fa08e62756
Oleg Kalnichevski <olegk@apache.org>
2010-10-16 17:25:54 +0000

    HTTPCLIENT-1014: ContentBody backed by a byte array Contributed by Axel Fontaine <business at axelfontaine.eu>
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@1023336 13f79535-47bb-0310-9956-ffa450edef68

---
+++ httpmime/src/main/java/org/apache/http/entity/mime/content/ByteArrayBody.java
@@ -0,0 +1,98 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.http.entity.mime.content;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.http.entity.mime.MIME;
+import org.apache.http.entity.mime.content.AbstractContentBody;
+
+/**
+ * Body part that is built using a byte array containing a file.
+ *
+ * @since 4.1
+ */
+public class ByteArrayBody extends AbstractContentBody {
+
+    /**
+     * The contents of the file contained in this part.
+     */
+    private final byte[] data;
+
+    /**
+     * The name of the file contained in this part.
+     */
+    private final String filename;
+
+    /**
+     * Creates a new ByteArrayBody.
+     *
+     * @param data The contents of the file contained in this part.
+     * @param mimeType The mime type of the file contained in this part.
+     * @param filename The name of the file contained in this part.
+     */
+    public ByteArrayBody(final byte[] data, final String mimeType, final String filename) {
+        super(mimeType);
+        if (data == null) {
+            throw new IllegalArgumentException("byte[] may not be null");
+        }
+        this.data = data;
+        this.filename = filename;
+    }
+
+    /**
+     * Creates a new ByteArrayBody.
+     *
+     * @param data The contents of the file contained in this part.
+     * @param filename The name of the file contained in this part.
+     */
+    public ByteArrayBody(final byte[] data, final String filename) {
+        this(data, "application/octet-stream", filename);
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void writeTo(final OutputStream out) throws IOException {
+        out.write(data);
+    }
+
+    public String getCharset() {
+        return null;
+    }
+
+    public String getTransferEncoding() {
+        return MIME.ENC_BINARY;
+    }
+
+    public long getContentLength() {
+        return data.length;
+    }
+
+}
