4c24320359c8b044bbf114bc3bae6c5a717ed448
Sebastian Bazley <sebb@apache.org>
2010-12-21 22:23:59 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@1051680 13f79535-47bb-0310-9956-ffa450edef68

--- httpmime/src/main/java/org/apache/http/entity/mime/HttpMultipartMode.java
+++ httpmime/src/main/java/org/apache/http/entity/mime/HttpMultipartMode.java
@@ -33,7 +33,9 @@ package org.apache.http.entity.mime;
  */
 public enum HttpMultipartMode {
 
+    /** RFC 822, RFC 2045, RFC 2046 compliant */
     STRICT,
+    /** browser-compatible mode, i.e. only write Content-Disposition; use content charset */
     BROWSER_COMPATIBLE
 
 }
--- httpmime/src/main/java/org/apache/http/entity/mime/MIME.java
+++ httpmime/src/main/java/org/apache/http/entity/mime/MIME.java
@@ -42,6 +42,7 @@ public final class MIME {
     public static final String ENC_8BIT              = "8bit";
     public static final String ENC_BINARY            = "binary";
 
+    /** The default character set to be used, i.e. "US-ASCII" */
     public static final Charset DEFAULT_CHARSET      = Charset.forName("US-ASCII");
 
 }
