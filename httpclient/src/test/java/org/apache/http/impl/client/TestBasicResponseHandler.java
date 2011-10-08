58e85071088ea322fff382e9bb5af00fbd391809
Oleg Kalnichevski <olegk@apache.org>
2011-10-08 11:57:25 +0000

    Fixed broken test case
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/branches/4.1.x@1180365 13f79535-47bb-0310-9956-ffa450edef68

--- httpclient/src/test/java/org/apache/http/impl/client/TestBasicResponseHandler.java
+++ httpclient/src/test/java/org/apache/http/impl/client/TestBasicResponseHandler.java
@@ -27,7 +27,6 @@
 
 package org.apache.http.impl.client;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import junit.framework.Assert;
@@ -51,7 +50,7 @@ public class TestBasicResponseHandler {
     public void testSuccessfulResponse() throws Exception {
         StatusLine sl = new BasicStatusLine(HttpVersion.HTTP_1_1, 200, "OK");
         HttpResponse response = Mockito.mock(HttpResponse.class);
-        HttpEntity entity = StringEntity.create("stuff");
+        HttpEntity entity = new StringEntity("stuff");
         Mockito.when(response.getStatusLine()).thenReturn(sl);
         Mockito.when(response.getEntity()).thenReturn(entity);
 
