d695c0e8b69ac48986cbd8ddba3781f524940c21
Oleg Kalnichevski <olegk@apache.org>
2012-12-28 14:59:30 +0000

    Updated multipart POST example
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@1426534 13f79535-47bb-0310-9956-ffa450edef68

--- httpmime/src/examples/org/apache/http/examples/entity/mime/ClientMultipartFormPost.java
+++ httpmime/src/examples/org/apache/http/examples/entity/mime/ClientMultipartFormPost.java
@@ -29,13 +29,14 @@ package org.apache.http.examples.entity.mime;
 import java.io.File;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 /**
@@ -48,13 +49,13 @@ public class ClientMultipartFormPost {
             System.out.println("File path not given");
             System.exit(1);
         }
-        HttpClient httpclient = new DefaultHttpClient();
+        CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
             HttpPost httppost = new HttpPost("http://localhost:8080" +
                     "/servlets-examples/servlet/RequestInfoExample");
 
             FileBody bin = new FileBody(new File(args[0]));
-            StringBody comment = new StringBody("A binary file of some kind");
+            StringBody comment = new StringBody("A binary file of some kind", ContentType.TEXT_PLAIN);
 
             MultipartEntity reqEntity = new MultipartEntity();
             reqEntity.addPart("bin", bin);
@@ -63,17 +64,20 @@ public class ClientMultipartFormPost {
             httppost.setEntity(reqEntity);
 
             System.out.println("executing request " + httppost.getRequestLine());
-            HttpResponse response = httpclient.execute(httppost);
-            HttpEntity resEntity = response.getEntity();
-
-            System.out.println("----------------------------------------");
-            System.out.println(response.getStatusLine());
-            if (resEntity != null) {
-                System.out.println("Response content length: " + resEntity.getContentLength());
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            try {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                HttpEntity resEntity = response.getEntity();
+                if (resEntity != null) {
+                    System.out.println("Response content length: " + resEntity.getContentLength());
+                }
+                EntityUtils.consume(resEntity);
+            } finally {
+                response.close();
             }
-            EntityUtils.consume(resEntity);
         } finally {
-            try { httpclient.getConnectionManager().shutdown(); } catch (Exception ignore) {}
+            httpclient.close();
         }
     }
 
