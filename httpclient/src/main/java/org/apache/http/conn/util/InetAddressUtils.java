cc250ec0216c67dbc3a0cbf910235f57e664b5fc
Sebastian Bazley <sebb@apache.org>
2013-04-04 14:10:46 +0000

    Trailing white space
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/branches/4.2.x@1464560 13f79535-47bb-0310-9956-ffa450edef68

--- httpclient/src/main/java/org/apache/http/conn/util/InetAddressUtils.java
+++ httpclient/src/main/java/org/apache/http/conn/util/InetAddressUtils.java
@@ -55,8 +55,8 @@ public class InetAddressUtils {
                 "^((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)$");
 
     /*
-     *  The above pattern is not totally rigorous as it allows for more than 7 hex fields in total 
-     */   
+     *  The above pattern is not totally rigorous as it allows for more than 7 hex fields in total
+     */
     private static final char COLON_CHAR = ':';
 
     // Must not have more than 7 colons (i.e. 8 fields)
@@ -64,7 +64,7 @@ public class InetAddressUtils {
 
     /**
      * Checks whether the parameter is a valid IPv4 address
-     * 
+     *
      * @param input the address string to check for validity
      * @return true if the input parameter is a valid IPv4 address
      */
@@ -74,7 +74,7 @@ public class InetAddressUtils {
 
     /**
      * Checks whether the parameter is a valid standard (non-compressed) IPv6 address
-     * 
+     *
      * @param input the address string to check for validity
      * @return true if the input parameter is a valid standard (non-compressed) IPv6 address
      */
@@ -84,7 +84,7 @@ public class InetAddressUtils {
 
     /**
      * Checks whether the parameter is a valid compressed IPv6 address
-     * 
+     *
      * @param input the address string to check for validity
      * @return true if the input parameter is a valid compressed IPv6 address
      */
@@ -100,7 +100,7 @@ public class InetAddressUtils {
 
     /**
      * Checks whether the parameter is a valid IPv6 address (including compressed).
-     * 
+     *
      * @param input the address string to check for validity
      * @return true if the input parameter is a valid standard or compressed IPv6 address
      */
