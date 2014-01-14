b840ce4951113c476840da12e00122e33a346815
Gary D. Gregory <ggregory@apache.org>
2014-01-14 14:38:23 +0000

    Simplify: no need for extra branch.
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@1558049 13f79535-47bb-0310-9956-ffa450edef68

--- httpclient/src/main/java/org/apache/http/impl/conn/DefaultSchemePortResolver.java
+++ httpclient/src/main/java/org/apache/http/impl/conn/DefaultSchemePortResolver.java
@@ -47,15 +47,14 @@ public class DefaultSchemePortResolver implements SchemePortResolver {
         final int port = host.getPort();
         if (port > 0) {
             return port;
+        }
+        final String name = host.getSchemeName();
+        if (name.equalsIgnoreCase("http")) {
+            return 80;
+        } else if (name.equalsIgnoreCase("https")) {
+            return 443;
         } else {
-            final String name = host.getSchemeName();
-            if (name.equalsIgnoreCase("http")) {
-                return 80;
-            } else if (name.equalsIgnoreCase("https")) {
-                return 443;
-            } else {
-                throw new UnsupportedSchemeException(name + " protocol is not supported");
-            }
+            throw new UnsupportedSchemeException(name + " protocol is not supported");
         }
     }
 
