e4157e5d24175f40319ff36fa65e498e6f8fc0c4
Oleg Kalnichevski <olegk@apache.org>
2015-05-20 09:20:04 +0000

    HTTPCLIENT-1648: Lax redirect strategy to allow redirects with DELETE method
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/trunk@1680499 13f79535-47bb-0310-9956-ffa450edef68

--- httpclient/src/main/java/org/apache/http/impl/client/LaxRedirectStrategy.java
+++ httpclient/src/main/java/org/apache/http/impl/client/LaxRedirectStrategy.java
@@ -28,6 +28,7 @@
 package org.apache.http.impl.client;
 
 import org.apache.http.annotation.Immutable;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpPost;
@@ -49,7 +50,8 @@ public class LaxRedirectStrategy extends DefaultRedirectStrategy {
     private static final String[] REDIRECT_METHODS = new String[] {
         HttpGet.METHOD_NAME,
         HttpPost.METHOD_NAME,
-        HttpHead.METHOD_NAME
+        HttpHead.METHOD_NAME,
+        HttpDelete.METHOD_NAME
     };
 
     @Override
