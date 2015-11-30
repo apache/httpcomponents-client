1c151cf38ac87dc7799630a038cd0c1bcce6920e
Oleg Kalnichevski <olegk@apache.org>
2015-11-30 20:34:24 +0000

    HTTPCLIENT-1550: fixed 'deflate' zlib header check
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpclient/branches/4.5.x@1717323 13f79535-47bb-0310-9956-ffa450edef68

--- httpclient/src/main/java/org/apache/http/client/entity/DeflateInputStream.java
+++ httpclient/src/main/java/org/apache/http/client/entity/DeflateInputStream.java
@@ -29,179 +29,110 @@ package org.apache.http.client.entity;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PushbackInputStream;
-import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 import java.util.zip.InflaterInputStream;
+import java.util.zip.ZipException;
 
-/** Deflate input stream.    This class includes logic needed for various Rfc's in order
-* to reasonably implement the "deflate" compression style.
-*/
-public class DeflateInputStream extends InputStream
-{
+/**
+ * Deflate input stream.    This class includes logic needed for various Rfc's in order
+ * to reasonably implement the "deflate" compression style.
+ */
+public class DeflateInputStream extends InputStream {
+
     private InputStream sourceStream;
 
-    public DeflateInputStream(final InputStream wrapped)
-        throws IOException
-    {
-        /*
-            * A zlib stream will have a header.
-            *
-            * CMF | FLG [| DICTID ] | ...compressed data | ADLER32 |
-            *
-            * * CMF is one byte.
-            *
-            * * FLG is one byte.
-            *
-            * * DICTID is four bytes, and only present if FLG.FDICT is set.
-            *
-            * Sniff the content. Does it look like a zlib stream, with a CMF, etc? c.f. RFC1950,
-            * section 2.2. http://tools.ietf.org/html/rfc1950#page-4
-            *
-            * We need to see if it looks like a proper zlib stream, or whether it is just a deflate
-            * stream. RFC2616 calls zlib streams deflate. Confusing, isn't it? That's why some servers
-            * implement deflate Content-Encoding using deflate streams, rather than zlib streams.
-            *
-            * We could start looking at the bytes, but to be honest, someone else has already read
-            * the RFCs and implemented that for us. So we'll just use the JDK libraries and exception
-            * handling to do this. If that proves slow, then we could potentially change this to check
-            * the first byte - does it look like a CMF? What about the second byte - does it look like
-            * a FLG, etc.
-            */
+    public DeflateInputStream(final InputStream wrapped) throws IOException {
 
-        /* We read a small buffer to sniff the content. */
-        final byte[] peeked = new byte[6];
-
-        final PushbackInputStream pushback = new PushbackInputStream(wrapped, peeked.length);
-
-        final int headerLength = pushback.read(peeked);
-
-        if (headerLength == -1) {
-            throw new IOException("Unable to read the response");
+        final PushbackInputStream pushback = new PushbackInputStream(wrapped, 2);
+        final int i1 = pushback.read();
+        final int i2 = pushback.read();
+        if (i1 == -1 || i2 == -1) {
+            throw new ZipException("Unexpected end of stream");
         }
 
-        /* We try to read the first uncompressed byte. */
-        final byte[] dummy = new byte[1];
+        pushback.unread(i2);
+        pushback.unread(i1);
 
-        final Inflater inf = new Inflater();
-
-        try {
-            int n;
-            while ((n = inf.inflate(dummy)) == 0) {
-                if (inf.finished()) {
-
-                    /* Not expecting this, so fail loudly. */
-                    throw new IOException("Unable to read the response");
-                }
-
-                if (inf.needsDictionary()) {
-
-                    /* Need dictionary - then it must be zlib stream with DICTID part? */
-                    break;
-                }
-
-                if (inf.needsInput()) {
-                    inf.setInput(peeked);
-                }
-            }
-
-            if (n == -1) {
-                throw new IOException("Unable to read the response");
-            }
-
-            /*
-                * We read something without a problem, so it's a valid zlib stream. Just need to reset
-                * and return an unused InputStream now.
-                */
-            pushback.unread(peeked, 0, headerLength);
-            sourceStream = new DeflateStream(pushback, new Inflater());
-        } catch (final DataFormatException e) {
-
-            /* Presume that it's an RFC1951 deflate stream rather than RFC1950 zlib stream and try
-                * again. */
-            pushback.unread(peeked, 0, headerLength);
-            sourceStream = new DeflateStream(pushback, new Inflater(true));
-        } finally {
-            inf.end();
+        boolean nowrap = true;
+        final int b1 = i1 & 0xFF;
+        final int compressionMethod = b1 & 0xF;
+        final int compressionInfo = b1 >> 4 & 0xF;
+        final int b2 = i2 & 0xFF;
+        if (compressionMethod == 8 && compressionInfo <= 7 && ((b1 << 8) | b2) % 31 == 0) {
+            nowrap = false;
         }
-
+        sourceStream = new DeflateStream(pushback, new Inflater(nowrap));
     }
 
-    /** Read a byte.
-    */
+    /**
+     * Read a byte.
+     */
     @Override
-    public int read()
-        throws IOException
-    {
+    public int read() throws IOException {
         return sourceStream.read();
     }
 
-    /** Read lots of bytes.
-    */
+    /**
+     * Read lots of bytes.
+     */
     @Override
-    public int read(final byte[] b)
-        throws IOException
-    {
+    public int read(final byte[] b) throws IOException {
         return sourceStream.read(b);
     }
 
-    /** Read lots of specific bytes.
-    */
+    /**
+     * Read lots of specific bytes.
+     */
     @Override
-    public int read(final byte[] b, final int off, final int len)
-        throws IOException
-    {
-        return sourceStream.read(b,off,len);
+    public int read(final byte[] b, final int off, final int len) throws IOException {
+        return sourceStream.read(b, off, len);
     }
 
-    /** Skip
-    */
+    /**
+     * Skip
+     */
     @Override
-    public long skip(final long n)
-        throws IOException
-    {
+    public long skip(final long n) throws IOException {
         return sourceStream.skip(n);
     }
 
-    /** Get available.
-    */
+    /**
+     * Get available.
+     */
     @Override
-    public int available()
-        throws IOException
-    {
+    public int available() throws IOException {
         return sourceStream.available();
     }
 
-    /** Mark.
-    */
+    /**
+     * Mark.
+     */
     @Override
-    public void mark(final int readLimit)
-    {
+    public void mark(final int readLimit) {
         sourceStream.mark(readLimit);
     }
 
-    /** Reset.
-    */
+    /**
+     * Reset.
+     */
     @Override
-    public void reset()
-        throws IOException
-    {
+    public void reset() throws IOException {
         sourceStream.reset();
     }
 
-    /** Check if mark is supported.
-    */
+    /**
+     * Check if mark is supported.
+     */
     @Override
-    public boolean markSupported()
-    {
+    public boolean markSupported() {
         return sourceStream.markSupported();
     }
 
-    /** Close.
-    */
+    /**
+     * Close.
+     */
     @Override
-    public void close()
-        throws IOException
-    {
+    public void close() throws IOException {
         sourceStream.close();
     }
 
