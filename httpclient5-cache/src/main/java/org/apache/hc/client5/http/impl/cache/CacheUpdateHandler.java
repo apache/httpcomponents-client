5f2ad23ab3a3879be616bc33a98e90192ed4f4bb
Sudheera Palihakkara <catchsudheera@gmail.com>
2018-01-15 18:08:58 +0800

    HTTPCLIENT-1690: Avoid updating the cache entry with Content-Encoding headers too. (missing piece from last commit). Updated the test case

--- httpclient5-cache/src/main/java/org/apache/hc/client5/http/impl/cache/CacheUpdateHandler.java
+++ httpclient5-cache/src/main/java/org/apache/hc/client5/http/impl/cache/CacheUpdateHandler.java
@@ -163,6 +163,10 @@ class CacheUpdateHandler {
         }
         for (final Iterator<Header> it = response.headerIterator(); it.hasNext(); ) {
             final Header responseHeader = it.next();
+            // Since we do not expect a content in a 304 response, should update the cache entry with Content-Encoding
+            if (HttpHeaders.CONTENT_ENCODING.equals(responseHeader.getName())) {
+                continue;
+            }
             headerGroup.addHeader(responseHeader);
         }
         return headerGroup.getAllHeaders();
