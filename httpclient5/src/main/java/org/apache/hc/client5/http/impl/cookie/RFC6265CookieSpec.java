7875ab19df790a09e84e06a1cf156d312daea352
Gary Gregory <garydgregory@gmail.com>
2018-07-12 09:06:57 -0600

    Use "String.isEmpty()" instead of "String.length() == 0"

--- httpclient5/src/main/java/org/apache/hc/client5/http/impl/cookie/RFC6265CookieSpec.java
+++ httpclient5/src/main/java/org/apache/hc/client5/http/impl/cookie/RFC6265CookieSpec.java
@@ -129,7 +129,7 @@ public class RFC6265CookieSpec implements CookieSpec {
             cursor = new ParserCursor(0, buffer.length());
         }
         final String name = tokenParser.parseToken(buffer, cursor, TOKEN_DELIMS);
-        if (name.length() == 0) {
+        if (name.isEmpty()) {
             return Collections.emptyList();
         }
         if (cursor.atEnd()) {
