09acfd340d04634dc339772f31d06365933bab1e
Michael Osipov <michaelo@apache.org>
2018-12-23 21:18:13 +0100

    Improve HttpResponseException#getMessage()
    The #getMessage() now properly consists of the status code as such and the
    optional reason phrase. Moreover, the pure reason phrase can be retrieved
    via #getReasonPhrase.

--- httpclient5/src/main/java/org/apache/hc/client5/http/HttpResponseException.java
+++ httpclient5/src/main/java/org/apache/hc/client5/http/HttpResponseException.java
@@ -38,14 +38,21 @@ public class HttpResponseException extends ClientProtocolException {
     private static final long serialVersionUID = -7186627969477257933L;
 
     private final int statusCode;
+    private final String reasonPhrase;
 
-    public HttpResponseException(final int statusCode, final String s) {
-        super(TextUtils.isBlank(s) ? Integer.toString(statusCode) : s);
+    public HttpResponseException(final int statusCode, final String reasonPhrase) {
+        super(String.format("status code: %d" +
+                (TextUtils.isBlank(reasonPhrase) ? "" : ", reason phrase: %s"), statusCode, reasonPhrase));
         this.statusCode = statusCode;
+        this.reasonPhrase = reasonPhrase;
     }
 
     public int getStatusCode() {
         return this.statusCode;
+    }
+
+    public String getReasonPhrase() {
+        return this.reasonPhrase;
     }
 
 }
