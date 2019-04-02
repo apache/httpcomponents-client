06d565aca39f9267799739a7da59c53623f97a96
Oleg Kalnichevski <olegk@apache.org>
2019-04-02 11:21:37 +0200

    [HTTPCORE-572] Move examples to the src/test folders for each module

---
+++ httpclient5/src/test/java/org/apache/hc/client5/http/examples/ClientConnectionRelease.java
@@ -0,0 +1,74 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.hc.client5.http.examples;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+
+/**
+ * This example demonstrates the recommended way of using API to make sure
+ * the underlying connection gets released back to the connection manager.
+ */
+public class ClientConnectionRelease {
+
+    public final static void main(final String[] args) throws Exception {
+        try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            final HttpGet httpget = new HttpGet("http://httpbin.org/get");
+
+            System.out.println("Executing request " + httpget.getMethod() + " " + httpget.getUri());
+            try (final CloseableHttpResponse response = httpclient.execute(httpget)) {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getCode() + " " + response.getReasonPhrase());
+
+                // Get hold of the response entity
+                final HttpEntity entity = response.getEntity();
+
+                // If the response does not enclose an entity, there is no need
+                // to bother about connection release
+                if (entity != null) {
+                    try (final InputStream inStream = entity.getContent()) {
+                        inStream.read();
+                        // do something useful with the response
+                    } catch (final IOException ex) {
+                        // In case of an IOException the connection will be released
+                        // back to the connection manager automatically
+                        throw ex;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
