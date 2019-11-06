8d2e70f2f2b511ac5a453c3c1466a8a39beab3fb
Olof Larsson <olof@sylt.nu>
2019-11-06 17:56:52 +0100

    HTTPCLIENT-2022: Use message parameter in HttpCacheEntrySerializationException constructor

--- httpclient-cache/src/main/java/org/apache/http/client/cache/HttpCacheEntrySerializationException.java
+++ httpclient-cache/src/main/java/org/apache/http/client/cache/HttpCacheEntrySerializationException.java
@@ -37,7 +37,7 @@ public class HttpCacheEntrySerializationException extends IOException {
     private static final long serialVersionUID = 9219188365878433519L;
 
     public HttpCacheEntrySerializationException(final String message) {
-        super();
+        super(message);
     }
 
     public HttpCacheEntrySerializationException(final String message, final Throwable cause) {
