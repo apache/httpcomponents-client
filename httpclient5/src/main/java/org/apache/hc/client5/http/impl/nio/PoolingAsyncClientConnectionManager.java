1572f5568730362aa4f5bdf052503db52c643bab
Carter Kozak <ckozak@apache.org>
2020-07-05 13:10:25 -0400

    HTTPCLIENT-2097: Fix PoolingAsyncClientConnectionManager boxed primitive reference equality

--- httpclient5/src/main/java/org/apache/hc/client5/http/impl/nio/PoolingAsyncClientConnectionManager.java
+++ httpclient5/src/main/java/org/apache/hc/client5/http/impl/nio/PoolingAsyncClientConnectionManager.java
@@ -252,7 +252,7 @@ public class PoolingAsyncClientConnectionManager implements AsyncClientConnectio
 
                                     @Override
                                     public void execute(final Boolean result) {
-                                        if (result == Boolean.FALSE) {
+                                        if (!result) {
                                             if (log.isDebugEnabled()) {
                                                 log.debug("{}: connection {} is stale", id, ConnPoolSupport.getId(connection));
                                             }
