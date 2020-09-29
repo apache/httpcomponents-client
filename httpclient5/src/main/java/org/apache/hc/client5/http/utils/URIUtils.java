894234a5aeb9958e7e466c383e4d0ded17a9a813
Oleg Kalnichevski <olegk@apache.org>
2020-09-29 09:37:38 +0200

    Incorrect handling of malformed authority component by URIUtils#extractHost

--- httpclient5/src/main/java/org/apache/hc/client5/http/utils/URIUtils.java
+++ httpclient5/src/main/java/org/apache/hc/client5/http/utils/URIUtils.java
@@ -250,56 +250,43 @@ public class URIUtils {
         if (uri == null) {
             return null;
         }
-        HttpHost target = null;
         if (uri.isAbsolute()) {
-            int port = uri.getPort(); // may be overridden later
-            String host = uri.getHost();
-            if (host == null) { // normal parse failed; let's do it ourselves
+            if (uri.getHost() == null) { // normal parse failed; let's do it ourselves
                 // authority does not seem to care about the valid character-set for host names
-                host = uri.getAuthority();
-                if (host != null) {
+                if (uri.getAuthority() != null) {
+                    String content = uri.getAuthority();
                     // Strip off any leading user credentials
-                    final int at = host.indexOf('@');
-                    if (at >= 0) {
-                        if (host.length() > at+1 ) {
-                            host = host.substring(at+1);
-                        } else {
-                            host = null; // @ on its own
-                        }
+                    int at = content.indexOf('@');
+                    if (at != -1) {
+                        content = content.substring(at + 1);
                     }
-                    // Extract the port suffix, if present
-                    if (host != null) {
-                        final int colon = host.indexOf(':');
-                        if (colon >= 0) {
-                            final int pos = colon + 1;
-                            int len = 0;
-                            for (int i = pos; i < host.length(); i++) {
-                                if (Character.isDigit(host.charAt(i))) {
-                                    len++;
-                                } else {
-                                    break;
-                                }
-                            }
-                            if (len > 0) {
-                                try {
-                                    port = Integer.parseInt(host.substring(pos, pos + len));
-                                } catch (final NumberFormatException ex) {
-                                }
-                            }
-                            host = host.substring(0, colon);
+                    final String scheme = uri.getScheme();
+                    final String hostname;
+                    final int port;
+                    at = content.indexOf(":");
+                    if (at != -1) {
+                        hostname = content.substring(0, at);
+                        try {
+                            final String portText = content.substring(at + 1);
+                            port = !TextUtils.isEmpty(portText) ? Integer.parseInt(portText) : -1;
+                        } catch (final NumberFormatException ex) {
+                            return null;
                         }
+                    } else {
+                        hostname = content;
+                        port = -1;
+                    }
+                    try {
+                        return new HttpHost(scheme, hostname, port);
+                    } catch (final IllegalArgumentException ex) {
+                        return null;
                     }
                 }
-            }
-            final String scheme = uri.getScheme();
-            if (!TextUtils.isBlank(host)) {
-                try {
-                    target = new HttpHost(scheme, host, port);
-                } catch (final IllegalArgumentException ignore) {
-                }
+            } else {
+                return new HttpHost(uri.getScheme(), uri.getHost(), uri.getPort());
             }
         }
-        return target;
+        return null;
     }
 
     /**
