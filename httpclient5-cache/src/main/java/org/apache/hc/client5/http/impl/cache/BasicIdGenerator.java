dca91083522269128bb9e4eb3e22f322677d8dca
Gary Gregory <garydgregory@gmail.com>
2022-07-06 10:56:04 -0400

    Use try-with-resources

--- httpclient5-cache/src/main/java/org/apache/hc/client5/http/impl/cache/BasicIdGenerator.java
+++ httpclient5-cache/src/main/java/org/apache/hc/client5/http/impl/cache/BasicIdGenerator.java
@@ -65,9 +65,9 @@ class BasicIdGenerator {
         final int rndnum = this.rnd.nextInt();
         buffer.append(System.currentTimeMillis());
         buffer.append('.');
-        final Formatter formatter = new Formatter(buffer, Locale.ROOT);
-        formatter.format("%1$016x-%2$08x", this.count, rndnum);
-        formatter.close();
+        try (Formatter formatter = new Formatter(buffer, Locale.ROOT)) {
+            formatter.format("%1$016x-%2$08x", this.count, rndnum);
+        }
         buffer.append('.');
         buffer.append(this.hostname);
     }
