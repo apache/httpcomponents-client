0df9e63932ea4dca63009ad52254bbb3fbf0b86e
Arturo Bernal <arturobernalg@gmail.com>
2023-03-11 22:23:44 +0100

    Enhance ContentResponseHandler to be fault-tolerant
    This commit enhances the `ContentResponseHandler` class to be more fault-tolerant by handling responses with null or empty bodies without throwing exceptions. If the response was successful (a 2xx status code), the `Content` object is returned. If no response body exists, `Content.NO_CONTENT` is returned.
    
    The implementation extends `ContentResponseHandler` and overrides its methods to handle the response entity and transform it into the actual response object. This is designed to be efficient and use minimal memory.

--- httpclient5/src/main/java/org/apache/hc/client5/http/HttpResponseException.java
+++ httpclient5/src/main/java/org/apache/hc/client5/http/HttpResponseException.java
@@ -26,6 +26,7 @@
  */
 package org.apache.hc.client5.http;
 
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.util.TextUtils;
 
 /**
@@ -39,13 +40,44 @@ public class HttpResponseException extends ClientProtocolException {
 
     private final int statusCode;
     private final String reasonPhrase;
+    private final byte[] contentBytes;
+    private final ContentType contentType;
 
+    /**
+     * Constructs a new instance of {@code HttpResponseException} with the given
+     * status code and reason phrase, and no content bytes or content type.
+     *
+     * @param statusCode   the HTTP status code
+     * @param reasonPhrase the reason phrase associated with the HTTP status code
+     */
     public HttpResponseException(final int statusCode, final String reasonPhrase) {
+        this(statusCode, reasonPhrase, null, null);
+    }
+
+    /**
+     * Constructs a new instance of {@code HttpResponseException} with the given
+     * status code, reason phrase, content bytes, and content type.
+     *
+     * @param statusCode   the HTTP status code
+     * @param reasonPhrase the reason phrase associated with the HTTP status code
+     * @param contentBytes the content bytes of the HTTP response
+     * @param contentType  the content type of the HTTP response
+     */
+    public HttpResponseException(final int statusCode, final String reasonPhrase, final byte[] contentBytes, final ContentType contentType) {
         super(String.format("status code: %d" +
-                (TextUtils.isBlank(reasonPhrase) ? "" : ", reason phrase: %s"), statusCode, reasonPhrase));
+                        (TextUtils.isBlank(reasonPhrase) ? "" : ", reason phrase: %s") +
+                        (contentBytes == null ? "" : ", content: %s"),
+                statusCode, reasonPhrase,
+                contentBytes == null || contentType == null || contentType.getCharset() == null ?
+                        null :
+                        new String(contentBytes, contentType.getCharset())));
+
         this.statusCode = statusCode;
         this.reasonPhrase = reasonPhrase;
+        this.contentBytes = contentBytes;
+        this.contentType = contentType;
     }
+
 
     public int getStatusCode() {
         return this.statusCode;
@@ -55,4 +87,11 @@ public class HttpResponseException extends ClientProtocolException {
         return this.reasonPhrase;
     }
 
+    public byte[] getContentBytes() {
+        return contentBytes;
+    }
+
+    public ContentType getContentType() {
+        return contentType;
+    }
 }
