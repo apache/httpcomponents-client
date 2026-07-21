ebac9512f555c4a355cad3f59ef2db69b597cc97
Oleg Kalnichevski <olegk@apache.org>
2026-07-21 10:32:34 +0200

    ContentCompressionExec fails to close the underlying response stream in case of an unexpected runtime or protocol exception

--- httpclient5/src/test/java/org/apache/hc/client5/http/impl/classic/TestContentCompressionExec.java
+++ httpclient5/src/test/java/org/apache/hc/client5/http/impl/classic/TestContentCompressionExec.java
@@ -202,7 +202,7 @@ class TestContentCompressionExec {
     @Test
     void testUnknownContentEncoding() throws Exception {
         final ClassicHttpRequest request = new BasicClassicHttpRequest(Method.GET, host, "/");
-        final ClassicHttpResponse response = new BasicClassicHttpResponse(200, "OK");
+        final ClassicHttpResponse response = Mockito.spy(new BasicClassicHttpResponse(200, "OK"));
         final HttpEntity original = EntityBuilder.create().setText("encoded stuff").setContentEncoding("whatever").build();
         response.setEntity(original);
 
@@ -212,6 +212,7 @@ class TestContentCompressionExec {
 
         Assertions.assertThrows(HttpException.class, () ->
                 impl.execute(request, scope, execChain));
+        Mockito.verify(response).close();
     }
 
     @Test
@@ -252,7 +253,7 @@ class TestContentCompressionExec {
         final HttpEntity entity = response1.getEntity();
         Assertions.assertNotNull(entity);
 
-        final ClassicHttpResponse response2 = new BasicClassicHttpResponse(200, "OK");
+        final ClassicHttpResponse response2 = Mockito.spy(new BasicClassicHttpResponse(200, "OK"));
         final HttpEntity original2 = EntityBuilder.create()
                 .setText("encoded stuff")
                 .setContentEncoding("gzip,gzip,gzip,gzip,gzip,gzip")
@@ -261,8 +262,10 @@ class TestContentCompressionExec {
 
         Mockito.when(execChain.proceed(request, scope)).thenReturn(response2);
 
-        final ProtocolException exception = Assertions.assertThrows(ProtocolException.class, () -> impl.execute(request, scope, execChain));
+        final ProtocolException exception = Assertions.assertThrows(ProtocolException.class, () ->
+                impl.execute(request, scope, execChain));
         Assertions.assertEquals("Codec list exceeds maximum of 5 elements", exception.getMessage());
+        Mockito.verify(response2).close();
     }
 
 }
