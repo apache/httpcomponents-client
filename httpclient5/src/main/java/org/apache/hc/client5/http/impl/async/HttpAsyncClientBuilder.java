f458fea5ad1158c6739cc5c5ccbcca6f550e4635
Arturo Bernal <abernal@apache.org>
2026-09-18 10:24:17 +0200

    HTTPCLIENT-2434 Deprecate idle connection eviction
    Recommend connection keep-alive management and expired connection eviction instead.

--- httpclient5/src/main/java/org/apache/hc/client5/http/impl/async/HttpAsyncClientBuilder.java
+++ httpclient5/src/main/java/org/apache/hc/client5/http/impl/async/HttpAsyncClientBuilder.java
@@ -857,7 +857,11 @@ public class HttpAsyncClientBuilder {
      * get closed and evicted from the pool.
      *
      * @return this instance.
+     * @deprecated Configure connection keep-alive settings appropriately and use
+     * {@link #evictExpiredConnections()} instead.
+     * @see RequestConfig#getConnectionKeepAlive()
      */
+    @Deprecated
     public final HttpAsyncClientBuilder evictIdleConnections(final TimeValue maxIdleTime) {
         this.evictIdleConnections = true;
         this.maxIdleTime = Args.notNull(maxIdleTime, "Max idle time");
